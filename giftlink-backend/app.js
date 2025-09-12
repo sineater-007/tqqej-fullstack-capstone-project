01ec6fc85db50340e103b51f39a9053894264d56
name <yourgithub@email.com>
2025-09-12 17:12:37 -0400

    Adding temporary changes to Github

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -29,11 +29,14 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+const authRoutes = require('./routes/authRoutes');
+
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
+
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
@@ -41,6 +44,8 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
