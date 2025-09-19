c87dce639e197c2e838590d5a4b49832957804b6
name <yourgithub@email.com>
2025-09-19 12:23:32 -0400

    Adding temporary changes to Github

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,58 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import {urlConfig} from '../../config';//Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { useAppContext } from '../../context/AuthContext';//Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useNavigate } from 'react-router-dom';//Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
 
 function LoginPage() {
-
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');//Task 4: Include a state for incorrect password.
+
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext(); //Task 5: Create a local variable for `navigate`,`bearerToken`   and `setIsLoggedIn`.
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+    }, [navigate])//Task 6. If the bearerToken has a value (user already logged in), navigate to MainPage
+
     // insert code here to create handleLogin function and include console.log
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
+        try{
+            //first task
+            const res = await fetch(`/api/auth/login`, {
+            method: 'POST',//Task 7: Set method
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            },//Task 8: Set headers
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })//Task 9: Set body to send user details
+            });
+            const json = await res.json();// Task 1: Access data coming from fetch API
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);// Task 2: Set user details
+                setIsLoggedIn(true);// Task 3: Set the user's state to log in using the `useAppContext`.
+                navigate('/app');// Task 4: Navigate to the MainPage after logging in.
+            } else {
+                document.getElementById("email").value="";
+                document.getElementById("password").value="";
+                setIncorrect("Wrong password. Try again.");
+                //Below is optional, but recommended - Clear out error message after 2 seconds
+                setTimeout(() => {
+                    setIncorrect("");
+                }, 2000);
+            }// Task 5: Clear input and set an error message if the password is incorrect
+         }catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     }
 
         return (
@@ -27,7 +71,7 @@ function LoginPage() {
                     className="form-control"
                     placeholder="Enter your email"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                 />
             </div>
 
@@ -39,8 +83,9 @@ function LoginPage() {
                         className="form-control"
                         placeholder="Enter your password"
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                     />
+                <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>// Task 6: Display an error message to the user.
             </div>
           {/* insert code here to create a button that performs the `handleLogin` function on click */}
             <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
